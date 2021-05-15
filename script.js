5da460013379bf16d65ca1b9047e26e67da15390
DHRUMILPARIKH <parikhdhrumil1996@gmail.com>
2021-05-15 10:20:59 +0530

    script.js: add logic of checkWinnerOfGame, index.html: add gameOver and gameWinner block

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ var blocks = [
   "cell_21",
   "cell_22",
 ];
-document.getElementById("h1_id").innerHTML = "Tics Tac Toe";
+var tables = {
+  rows: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ],
+  colls: [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ],
+  diagnoals: [
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+};
+var states = [];
+var selected = 0;
+var gameIsOver = false;
+document.getElementById("game_heading").innerHTML = "Tics Tac Toe";
 document.getElementById("now_turn").innerHTML = symbols[0];
 
 /**
@@ -20,22 +39,38 @@ document.getElementById("now_turn").innerHTML = symbols[0];
  * @param {string} id id of button tag
  */
 function onCellClick(id) {
+  // 0; Check game is over or not
+  if (gameIsOver) return;
   // 1: Check cell is previously selected or not
   var is_data_exist = document.getElementById(id).innerText;
+  var currnetMove = symbols[nextTurn];
   if (is_data_exist === symbols[0] || is_data_exist === symbols[1]) {
     return;
   }
   // 2: Set value on cell and change in nextTurn
   document.getElementById(id).innerHTML = symbols[nextTurn];
+  selected += 1;
+  var index = blocks.findIndex((block) => block === id);
+  states[index] = symbols[nextTurn];
   if (nextTurn == 0) {
     nextTurn = 1;
   } else {
     nextTurn = 0;
   }
-  /**
-   *  TODO: Check winner of the game
-   */
-  document.getElementById("now_turn").innerHTML = symbols[nextTurn];
+  // 3:  Check state of the game
+  var stateOfGame = checkWinnerOfGame();
+  if (selected === 9 && !stateOfGame) {
+    document.getElementById("game_over").innerHTML = "Game Over";
+    document.getElementById("now_turn").innerHTML = "";
+    document.getElementById("game_winner").innerHTML = "DRAW";
+  } else if (stateOfGame) {
+    gameIsOver = true;
+    document.getElementById("game_over").innerHTML = "Game Over";
+    document.getElementById("now_turn").innerHTML = "";
+    document.getElementById("game_winner").innerHTML = currnetMove + " is WIN";
+  } else {
+    document.getElementById("now_turn").innerHTML = symbols[nextTurn];
+  }
 }
 
 /**
@@ -45,16 +80,52 @@ function onCellClick(id) {
  */
 function checkWinnerOfGame() {
   // 1: There is atlist 3 cell selected || return false
+  if (selected < 3) {
+    return false;
+  }
   // 2: Match the cells of row. matched && return true.
+  var checkRowBlock = matchTheBlocks("rows");
+  if (checkRowBlock) return true;
+
   // 3: Match the cells of collumn. matched && return true.
+  var checkCollBlock = matchTheBlocks("colls");
+  if (checkCollBlock) return true;
   // 4: Match the cells of diagonal. matched && return true.
+  var checkDiagonalBlock = matchTheBlocks("diagnoals");
+  if (checkDiagonalBlock) return true;
   // 5: return false
+  return false;
+}
+
+function matchTheBlocks(blockName) {
+  var isWinnerFound = false;
+  tables[blockName].forEach(([cell0, cell1, cell2]) => {
+    if (
+      isWinnerFound ||
+      !symbols.includes(states[cell0]) ||
+      !symbols.includes(states[cell1]) ||
+      !symbols.includes(states[cell2])
+    )
+      return;
+    if (states[cell0] === states[cell1] && states[cell1] === states[cell2]) {
+      isWinnerFound = true;
+      return;
+    }
+  });
+  return isWinnerFound;
 }
 
 /**
  * Reset the game
  */
 function clearGame() {
+  states = [];
+  selected = 0;
+  nextTurn = 0;
+  gameIsOver = false;
+  document.getElementById("game_over").innerHTML = "";
+  document.getElementById("now_turn").innerHTML = symbols[0];
+  document.getElementById("game_winner").innerHTML = "";
   for (i = 0; i < blocks.length; i++) {
     document.getElementById(blocks[i]).innerText = "";
   }
